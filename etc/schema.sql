a38d4891dccafdcd9c86afc2dce0596ebd204067
Justin Hawkins <justin@hawkins.id.au>
2014-05-17 13:31:30 +0930

    Tighten up SQL and add a 'hidden' field for data sets which should not be initially revealed.

--- etc/schema.sql
+++ etc/schema.sql
@@ -5,34 +5,36 @@ DROP TABLE challenges;
 DROP TABLE users;
 
 CREATE TABLE "users" (
-  id       INTEGER PRIMARY KEY AUTOINCREMENT,
-  username NOT NULL UNIQUE
+  id       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  username TEXT    NOT NULL UNIQUE
 );
 
 CREATE TABLE "challenges" (
-  id       INTEGER PRIMARY KEY AUTOINCREMENT,
-  name     TEXT NOT NULL,
-  descr    TEXT NOT NULL
+  id       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name     TEXT    NOT NULL,
+  descr    TEXT    NOT NULL
 );
 
 CREATE TABLE "languages" (
-  id       INTEGER PRIMARY KEY AUTOINCREMENT,
-  name     TEXT NOT NULL,
-  compile  TEXT,
-  run      TEXT
+  id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name        TEXT    NOT NULL,
+  compile     TEXT,
+  run         TEXT,
+  boilerplate TEXT
 );
 
 CREATE TABLE "challenge_data" (
-  id       INTEGER PRIMARY KEY AUTOINCREMENT,
-  challenge_id    INTEGER NOT NULL REFERENCES challenges(id),
-  input    TEXT NOT NULL,
-  output   TEXT NOT NULL
+  id             INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  challenge_id   INTEGER NOT NULL REFERENCES challenges(id),
+  input          TEXT    NOT NULL,
+  output         TEXT    NOT NULL,
+  hidden         BOOLEAN NOT NULL
 );
 
 CREATE TABLE "challenge_entries" (
-  id       INTEGER PRIMARY KEY AUTOINCREMENT,
-  challenge_id    INTEGER NOT NULL REFERENCES challenges(id),
-  user_id     INTEGER NOT NULL REFERENCES users(id),
-  language_id INTEGER NOT NULL REFERENCES languages(id),
-  code     TEXT NOT NULL
+  id            INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  challenge_id  INTEGER NOT NULL REFERENCES challenges(id),
+  user_id       INTEGER NOT NULL REFERENCES users(id),
+  language_id   INTEGER NOT NULL REFERENCES languages(id),
+  code          TEXT    NOT NULL
 );
