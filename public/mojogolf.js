11fb9c1decd893556e7824417bc867b93ebd4b2c
trentstollery <trent@cyberpirateninja.com>
2014-05-31 15:59:10 +0930

    Hooked up the lanag addtion form with things, Added a sexy alert box

--- public/mojogolf.js
+++ public/mojogolf.js
@@ -1,6 +1,51 @@
+
+function showalert(message,alerttype) {
+
+    $('#alert_placeholder').
+    append('<div id="alertdiv" class="alert ' +  alerttype + '"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+
+    setTimeout(function() {
+
+
+      $("#alertdiv").remove();
+
+    }, 5000);
+  }
+
 $( document ).ready(function() {
+
+  $(".alert").alert();
+
   $('#myTab a').click(function (e) {
-    console.log("triggered");
+    e.preventDefault()
     $(this).tab('show')
   });
+
+  /**
+  /rest/v1/languages
+  **/
+
+  $('#languageForm').submit(function(e){
+    e.preventDefault();
+    var dataArray = JSON.stringify({'name':$("#name",this).val()});
+    $.ajax({
+      type: "POST",
+      url: "/rest/v1/languages",
+      contentType:"application/json",
+      data: dataArray,
+      dataType:"json"
+    })
+    .done(function( msg ) {
+      showalert("Language Has Been Added with the ID:"+msg.id,"alert-success");
+    })
+    .fail(function(msg){
+      showalert("Woops, we were unable to add the langauge","alert-danger");
+    });
+
+  });
+
+
 });
+
+
+//alert-warning
